7b6497bb77d967cf18a8432dca910f54a6d81a19
John <libo@kembo-net.com>
2016-12-20 18:47:16 +0800

    signin

--- sorcery_demo/app/controllers/users_controller.rb
+++ sorcery_demo/app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  def index
+
+  end
+
   def new
     @user = User.new
   end
@@ -7,17 +11,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = 'success'
-      redirect_to root_path
+      # flash[:notice] = 'success'
+      flash[:success] = 'Welcome to the sample app!'
+      # redirect_to @user
+      redirect_to user_url(@user)
     else
       render 'new'
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+    redirect_to 'show'
+    debugger
+  end
 
   private
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
     end
 end
-
